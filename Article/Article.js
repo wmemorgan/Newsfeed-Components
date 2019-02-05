29fc4874884e54ab1cf43db896d5a0fb880003c5
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-02-05 14:50:17 +0800

    Add Javascript functionality to Article component

--- Article/Article.js
+++ Article/Article.js
@@ -1,20 +1,21 @@
 // Because classes are not hoised you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
 
 class Article {
-  constructor(domElement) {
+  constructor(newArticle) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.newArticle = newArticle;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = newArticle.querySelector('.expandButton')
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    this.expandButton.textContent = 'expand'
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+    this.expandButton.addEventListener('click', this.expandArticle.bind(this))
+    //this.expandButton.addEventListener('click', this.expandArticle)
   }
 
-  expandArticle() {
+  expandArticle () {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-
+    this.newArticle.classList.toggle('article-open')
   }
 }
 
@@ -26,4 +27,4 @@ class Article {
 
 */
 
-let articles;
+let articles = document.querySelectorAll('.article').forEach(article => new Article(article))
